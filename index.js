10c453dabe0e3c514911d44fe892dbb769e80a95
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-11-05 14:58:03 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+// 1-masala
+
+// var fruit=['Apple', 12,'Annanas', 'Orange', 'Bannana', 'Kivi'].
+// a=0;
+// b=0;
+// var title= fruit.join('')
+// for( i=0; i<title.length; i++){
+//    (title[i]==='a' || title[i]=== 'A') && a++
+//    (title[i]==='b' || title[i]=== 'B') && b++
+   
+//  }
+ 
+// console.log(a, '=a');
+// console.log(b, '=b');
+
+// 2-masala
+
+// var number=[10, 5, 11, 'apple', 2, 3, 4, 5, 6]
+// var c=0
+// for(i=0; i<number.length; i++){
+//     if(typeof number[i] === 'number'){
+//         c += number[i]
+
+//     }else continue
+// }
+// console.log(c);
+
+// 3-masala
+
+// 4-masala
+
+
+// var number=[10, 5, 11, 12, 2, 3, 4, 5, 6]
+ 
+// toq =0
+// juft=0
+// for (let i = 0; i < number.length; i++) {
+//     if(i%2===0){
+//         juft += number[i]
+//     }else if(i%2 != 0){
+//         toq += number[i]
+//     }
+// }
+// console.log(toq, '=toqson');
+// console.log(juft, '=juftson');
+
+// 5-masala
+
